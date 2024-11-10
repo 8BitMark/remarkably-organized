589fec05eeb02974aab889e9485815992854989e
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-11-10 21:51:05 +0000

    Update AgendaDay.svelte

--- src/lib/components/AgendaDay.svelte
+++ src/lib/components/AgendaDay.svelte
@@ -1,9 +1,10 @@
 
-		<div class="tasks">
+		<div class="shedule">
+     <div class="tasks">
 		       <medium>Priorities</medium>
-		</div>
+   		</div>
 
-		<div class="schedule">
+		<div class="hours">
 			{#each new Array(17) as _, i (i)}
 				<div class="hour-label">
 					{#if i > 0}
@@ -20,32 +21,33 @@
 		<div class="hour"></div>
 	{/each}
 </div>
+</div>
 
 <style lang="scss">
-	.tasks {
+	.schedule {
 		display: grid;
-		grid-template-columns: 1.5rem 1fr;
+		grid-template-columns: repeat(2, 1fr);
 		grid-template-rows: repeat(18, 1fr);
-		height: 50%;
-		width: 60%;
+		
+		.tasks {
+		height: 100%;
+		width: 100%;
 		justify-items: stretch;
 		align-items: left;
 		grid-auto-flow: column;
 		padding: 1rem 1rem 0 0;
 	}
 
-	.schedule {
-		display: grid;
-		grid-template-columns: 1rem 1fr;
-		grid-template-rows: repeat(18, 1fr);
-		height: 50%;
-		width: 40%;
+	.hours {
+		grid-column:2;
+		height: 100%;
+		width: 100%;
 		justify-items: stretch;
 		align-items: right;
 		grid-auto-flow: column;
 		padding: 1rem 1rem 0 0;
 	}
-
+}
 	.hour {
 		border-top: solid 1px var(--outline);
 	}
