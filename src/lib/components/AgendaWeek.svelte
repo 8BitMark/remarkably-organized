7e87da34110408c9cb75635ae08cbd0da728162a
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-23 23:32:40 +0000

    Update AgendaWeek.svelte

--- src/lib/components/AgendaWeek.svelte
+++ src/lib/components/AgendaWeek.svelte
@@ -168,6 +168,7 @@
      border-bottom: 2px solid var(--outline-high);
 					height: 25px;
      margin-top: 5px;
+					margin-bottom: 5px;
      font-size: 1.2em;
      color: var(--text-low);
      padding-top: 5px;
