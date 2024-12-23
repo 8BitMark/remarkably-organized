ef55bb31a4b6578ffd4b555684a717658734e8e9
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-23 14:32:29 +0000

    Update AgendaDay.svelte

--- src/lib/components/AgendaDay.svelte
+++ src/lib/components/AgendaDay.svelte
@@ -18,13 +18,13 @@
       <div class="schedule">
         <h2>Schedule</h2>
         {#each Array(17) as _, i (i)}
-        <div class="hour">
+        <div class="half-hour">
           {(i + 6)}
 <!--% 12 === 0 ? 12 : (i + 6) % 12} {i < 6 ? 'AM' : 'PM'}-->
         </div>
-        <div class="half-hour"></div>
+        <div class="hour"></div>
         {/each}
-	<div class="hour"></div>
+	<div class="half-hour"></div>
       </div>
     </div>
 </div>
