ab2257ee48d061a5e526908cfb12f54f222e830b
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-24 15:13:33 +0000

    date

--- src/lib/components/CalendarYear.svelte
+++ src/lib/components/CalendarYear.svelte
@@ -107,7 +107,7 @@
 							<div class="day" style:grid-column=1> X 
 							</div>			
 						{:else if startWeekOnSunday ? (month.start.getUTCDay() + day) % 7 === 0 : (month.start.getUTCDay() + day) % 7 === 1}
-							<div class="day" style:grid-column=1>{getISOWeekNumber(month.start.getUTCDate(), startWeekOnSunday)}</div>
+							<div class="day" style:grid-column=1>{getISOWeekNumber(month.start.getUTCDay() + 1 + day, startWeekOnSunday)}</div>
 						{/if}
 						<a class="day"
 							style:grid-column={day > 0
