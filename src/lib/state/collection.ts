e0e813d39fad5a4e33185a0cae80ceaf7aa3eeb2
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-24 11:41:44 +0000

    Update collection.ts

--- src/lib/state/collection.ts
+++ src/lib/state/collection.ts
@@ -38,7 +38,8 @@ export type PageTemplate =
 	| 'notes-week-rows'
 	| 'notes-day'
 	| 'habit-year-by-week'
-	| 'habit-year-by-month';
+	| 'habit-year-by-month'
+	| 'goals-quarter';
 
 export interface Collection {
 	/** The URL friendly slug used to link & id the collection */
