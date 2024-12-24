1618d3fcf20e8d852f2c53c8474d40753f02bf52
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-24 12:18:43 +0000

    Update CalendarYear.svelte

--- src/lib/components/CalendarYear.svelte
+++ src/lib/components/CalendarYear.svelte
@@ -69,6 +69,9 @@
 						<div class="label">{getDayShortName(0)}</div>
 					{/if}
 					{#each new Array(month.end.getUTCDate()) as _, day}
+						{#if ((month.start.getUTCDay() + day) % 7 === 1)}
+    							<div class="day">X</div>
+						{/if}
 						<div
 							class="day"
 							style:grid-column={day > 0
