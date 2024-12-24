7d8a6a0e1b086e44b529182b6dbb9afd4d9218bb
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-24 11:10:20 +0000

    Update CalendarYear.svelte

--- src/lib/components/CalendarYear.svelte
+++ src/lib/components/CalendarYear.svelte
@@ -69,7 +69,7 @@
 						<div class="label">{getDayShortName(0)}</div>
 					{/if}
 					{#each new Array(month.end.getUTCDate()) as _, day}
-						{#if startWeekOnSunday ? month.start.getUTCDay()+day === 0 : month.start.getUTCDay()+day === 1}
+						{#if month.start.getUTCDay()+day === 1}
  						   <div class="day">X</div>
 						{/if}
 						<div
