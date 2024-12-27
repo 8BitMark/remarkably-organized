8e4d8bedda0fac7b4ec53a2b6421b2a1a1296af5
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-27 19:22:02 +0000

    Update WeekNotes.svelte

--- src/routes/planner/WeekNotes.svelte
+++ src/routes/planner/WeekNotes.svelte
@@ -1,1 +1,37 @@
-f
+<script lang="ts">
+	import { PlannerSettings, intersect, type Week } from '$lib';
+	import Page from '$lib/components/Page.svelte';
+	import SideNav from './SideNav.svelte';
+	import TopNav from './TopNav.svelte';
+
+	let { week = {} as Week, settings = {} as PlannerSettings } = $props();
+</script>
+
+{#if settings.weekPage.notePagesAmount > 0}
+	{#each new Array(settings.weekPage.notePagesAmount) as _, i}
+		<article
+			id="{week.id}-pg{i + 2}"
+			use:intersect={{ rootMargin: '1000px 0px 1000px 0px' }}>
+			<SideNav {settings} tabs={settings.weekPage.sideNavDisplay} timeframe={week} />
+			<TopNav
+				{settings}
+				timeframe={week}
+				breadcrumbs={[{ href: `#${week.id}-pg${i + 2}`, name: `Page ${i + 2}` }]} />
+			<Page display={settings.weekPage.notePagesTemplate} {settings} timeframe={week} />
+		</article>
+	{/each}
+{/if}
+
+<style lang="scss">
+	article {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		padding-left: var(--sidenav-width);
+		padding-top: var(--topnav-height);
+	}
+	:global(main.side-nav-right) article {
+		padding-right: var(--sidenav-width);
+		padding-left: 0;
+	}
+</style>
