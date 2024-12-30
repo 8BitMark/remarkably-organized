609000bba4cb9aa61f552912e8e1ccd90a27a166
8BitMark <64683914+8BitMark@users.noreply.github.com>
2024-12-30 10:14:58 +0000

    Update TopNav.svelte

--- src/routes/planner/TopNav.svelte
+++ src/routes/planner/TopNav.svelte
@@ -170,9 +170,17 @@
 			{/if}
 		</ol>
 		<div style="flex: 1" />
-			<ol class="links">
-    {#if showDayBreadcrumb}
-     <a href="#{timeframe.year}-{timeframe.month}-{timeframe.daySinceMonth}-pg2" class="planner-button">
+			 {#if showWeekBreadcrumb}
+     <a href="#{timeframe.year}-wk{timeframe.weekSinceYear}-pg2">WeekNotes</a>
+    {:else if showDayBreadcrumb}
+     <a href="#{timeframe.year}-{timeframe.month}-{timeframe.daySinceMonth}" class="planner-button">
+					  <svg width="16" height="16" viewBox="0 0 24 24">
+					    <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+					    <line x1="16" y1="2" x2="16" y2="6"></line>
+					    <line x1="8" y1="2" x2="8" y2="6"></line>
+					    <line x1="3" y1="10" x2="21" y2="10"></line>
+					  </svg>Planner</a>
+      <a href="#{timeframe.year}-{timeframe.month}-{timeframe.daySinceMonth}-pg2" class="planner-button">
 					  <svg width="16" height="16" viewBox="0 0 24 24">
 					    <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
 					    <line x1="16" y1="2" x2="16" y2="6"></line>
@@ -180,10 +188,6 @@
 					    <line x1="3" y1="10" x2="21" y2="10"></line>
 					  </svg>Notes</a>
     {/if}
-    {#if showWeekBreadcrumb}
-     <li><a href="#{timeframe.year}-wk{timeframe.weekSinceYear}-pg2">WeekNotes</a></li>
-    {/if}
-			</ol>
 	</nav>
 {/if}
 
@@ -300,6 +304,7 @@
     text-decoration: none;
     font-family: Arial, sans-serif;
     font-size: 12px;
+				padding-top:10px;
   }
 
   .planner-button svg {
