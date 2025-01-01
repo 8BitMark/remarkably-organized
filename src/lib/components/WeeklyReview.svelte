d09b4969a86a484c1c5334d7e36aa6f38227a208
8BitMark <64683914+8BitMark@users.noreply.github.com>
2025-01-01 12:05:01 +0000

    Update WeeklyReview.svelte

--- src/lib/components/WeeklyReview.svelte
+++ src/lib/components/WeeklyReview.svelte
@@ -1,1 +1,24 @@
-gg
+<div class="container">
+    <div class="left-side">
+      <div class="notes">
+        <h2>Notes</h2>
+        {#each Array(26) as _, i}
+          <div class="lines"></div>
+        {/each}
+      </div>
+    </div>
+    <div class="right-side">
+      <div class="schedule">
+        <h2>Schedule</h2>
+        {#each Array(17) as _, i (i)}
+        <div class="hour">
+          {(i + 6)}
+<!--% 12 === 0 ? 12 : (i + 6) % 12} {i < 6 ? 'AM' : 'PM'}-->
+        </div>
+        <div class="half-hour"></div>
+        {/each}
+	<div class="hour"></div>
+	<div class="half-hour"></div>
+      </div>
+    </div>
+</div>
