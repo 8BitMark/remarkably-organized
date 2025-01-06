486290a83cf14d57ecdb6f8f5799fd06bc6303ba
8BitMark <64683914+8BitMark@users.noreply.github.com>
2025-01-06 01:00:36 +0000

    Update TopNav.svelte

--- src/routes/planner/TopNav.svelte
+++ src/routes/planner/TopNav.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { formatToString, PlannerSettings, type Timeframe } from '$lib';
-	import HomeIcon from '~icons/material+symbols+outlined/calendar_month';
+	import HomeIcon from '~icons/material-symbols-light/calendar-rounded';
 	import { getFontInfo } from '../fonts/fonts';
 
 	let {
