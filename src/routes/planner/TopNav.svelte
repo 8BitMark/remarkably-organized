a5eb97625ffbea2e00f4f69ca6c4badfe3921639
8BitMark <64683914+8BitMark@users.noreply.github.com>
2025-01-09 17:27:35 +0000

    Update TopNav.svelte

--- src/routes/planner/TopNav.svelte
+++ src/routes/planner/TopNav.svelte
@@ -217,7 +217,7 @@
 		
 	<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28">
 		<path fill="currentColor" d="M21.75 3A3.25 3.25 0 0 1 25 6.25v15.5A3.25 3.25 0 0 1 21.75 25H6.25A3.25 3.25 0 0 1 3 21.75V6.25A3.25 3.25 0 0 1 6.25 3zm1.75 6.503h-19V21.75c0 .966.784 1.75 1.75 1.75h15.5a1.75 1.75 0 0 0 1.75-1.75zM21.75 4.5H6.25A1.75 1.75 0 0 0 4.5 6.25v1.753h19V6.25a1.75 1.75 0 0 0-1.75-1.75" />
-		<text x="18" y="14" text-anchor="middle" fill="black" font-size="12">
+		<text x="14" y="8" text-anchor="middle" fill="black" font-size="12">
 						w{settings.weekPage.useWeekSinceYear
 							? timeframe.weekSinceYear
 							: timeframe.weekSinceMonth}</text>
