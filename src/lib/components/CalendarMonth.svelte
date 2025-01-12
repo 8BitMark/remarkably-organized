bf1dd92ad69057993d146b9c709d5a619f57dd04
8BitMark <64683914+8BitMark@users.noreply.github.com>
2025-01-12 09:06:12 +0000

    Update CalendarMonth.svelte

--- src/lib/components/CalendarMonth.svelte
+++ src/lib/components/CalendarMonth.svelte
@@ -159,7 +159,7 @@ transform: rotate(270deg);
 .week:nth-child(1) {
 border-top: solid 1px var(--outline-high);
 }
-.day:nth-child(-n + 10) {
+.day:nth-child(-n + 11) {
 border-top: solid 1px var(--outline-high);
 }
 		.events {
